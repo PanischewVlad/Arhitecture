8dcf66667b74a694c0791175582857913a99dfe3
Panishev <panishhev.vlad@gmail.com>
2020-12-09 18:28:44 +0200

    Fist commit home work7.12.20

--- app/src/main/java/com/test/architecture/MainActivity.java
+++ app/src/main/java/com/test/architecture/MainActivity.java
@@ -7,17 +7,8 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnStart; btnStart.setOnClickListener(view ->{
-        Intent intent = new Intent( this, SecondActivity.class);
-        startActivity(intent);
-    })
-    Button btnStart2; btnStart2.setOnClickListener(view ->
-
-    {
-        Intent intent = new Intent(this, ThirdActivity.class);
-        startActivity(intent);
-    })
-
+    Button btnStart2;
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         btnStart = findViewById(R.id.btnStart);
 
          btnStart2 = findViewById(R.id.btnStart2);
+         btnStart.setOnClickListener(view ->{
+            Intent intent = new Intent( this, SecondActivity.class);
+            startActivity(intent);
+        });
+         btnStart2.setOnClickListener(view ->{
+            Intent intent = new Intent(this, ThirdActivity.class);
+            startActivity(intent);
+        });
 
     }
+
 }
